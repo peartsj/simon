62380c8b185326de72ad2b28cfef29de688c0746
Spencer Peart <spencer.peart@gmail.com>
2023-03-22 21:57:34 -0600

    Update all files in public

--- public/play.js
+++ public/play.js
@@ -126,45 +126,57 @@ const btnDescriptions = [
       return buttons[Math.floor(Math.random() * this.buttons.size)];
     }
   
-    saveScore(score) {
+    async saveScore(score) {
       const userName = this.getPlayerName();
+      const date = new Date().toLocaleDateString();
+      const newScore = { name: userName, score: score, date: date };
+
+      try {
+        const response = await fetch('/api/score', {
+          method: 'POST',
+          headers: { 'content-type': 'application/json' },
+          body: JSON.stringify(newScore),
+        });
+
+        // Store what the service gave us as the high scores
+        const scores = await response.json();
+        localStorage.setItem('scores', JSON.stringify(scores));
+      } catch {
+        // If there was an error then just track scores locally
+        this.updateScoresLocal(newScore);
+      }
+    }
+  
+    updateScoresLocal(newScore) {
       let scores = [];
       const scoresText = localStorage.getItem('scores');
       if (scoresText) {
         scores = JSON.parse(scoresText);
       }
-      scores = this.updateScores(userName, score, scores);
-  
-      localStorage.setItem('scores', JSON.stringify(scores));
-    }
-  
-    updateScores(userName, score, scores) {
-      const date = new Date().toLocaleDateString();
-      const newScore = { name: userName, score: score, date: date };
-  
+
       let found = false;
       for (const [i, prevScore] of scores.entries()) {
-        if (score > prevScore.score) {
+        if (newScore > prevScore.score) {
           scores.splice(i, 0, newScore);
           found = true;
           break;
         }
       }
-  
+
       if (!found) {
         scores.push(newScore);
       }
-  
+
       if (scores.length > 10) {
         scores.length = 10;
       }
-  
-      return scores;
+
+      localStorage.setItem('scores', JSON.stringify(scores));
     }
   }
-  
+
   const game = new Game();
-  
+
   function delay(milliseconds) {
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -172,7 +184,7 @@ const btnDescriptions = [
       }, milliseconds);
     });
   }
-  
+
   function loadSound(filename) {
     return new Audio('assets/' + filename);
   }
